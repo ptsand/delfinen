9c30eb7f584c0e2a975534995bdd13d5e2971768
Peter <ptrsand@gmail.com>
2021-05-21 13:19:48 +0200

    Rettet stavefejl

--- src/model/KonkurrenceSvømmer.java
+++ src/model/KonkurrenceSvømmer.java
@@ -2,7 +2,6 @@ package model;
 
 public class KonkurrenceSvømmer extends Medlem {
 
-
     public KonkurrenceSvømmer(String navn, String tlf, String email) {
         super(navn, tlf, email);
     }
